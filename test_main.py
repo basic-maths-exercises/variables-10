cef9560fcb9c8872646929073473f440394ca261
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-24 18:13:39 +0100

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import unittest
+import main
+
+class UnitTests(unittest.TestCase) :
+    def test_arrayValues(self) : 
+       for j in range(11) : 
+          self.assertTrue( timesTable[j]==j*table, "One of more of the values in the timesTable array has not been set correctly" )
+          
+    def test_variablesExist(self) : 
+       self.assertTrue( "table" in globals(), "no variable called table has been defined" )
+       self.assertTrue( "timesTable" in globals(), "no variable called timesTable has been defined" )
+       self.assertTrue( len(timesTable)==11, "the timesTable array has the wrong length" )
+       
+   def test_variable_used(self) : 
+       self.assertTrue( "i" in globals(), "the loop vairable must be called i" )
+       self.assertTrue( i==10, "the loop variable must be called i" )
